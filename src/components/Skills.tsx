70df91aa6a8ee71b7ce3bd7198b28e5d6e136c38
Ayush Salunkhe <139464443+ayushsalunkhe@users.noreply.github.com>
2024-12-16 12:38:24 +0530

    Update Skills.tsx

--- src/components/Skills.tsx
+++ src/components/Skills.tsx
@@ -1,5 +1,5 @@
 import { motion } from 'framer-motion'
-import { Code2, Layout, Server, Database, Brush, Workflow } from 'lucide-react'
+import { Layout, Server, Workflow } from 'lucide-react'
 
 const Skills = () => {
   const skillCategories = [
